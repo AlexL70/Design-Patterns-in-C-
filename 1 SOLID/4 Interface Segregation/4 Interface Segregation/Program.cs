0e6f6433974ac4c7d8c38fc3916f9d849aee0088
AlexL70 <alexander.levinson.70@gmail.com>
2018-07-22 17:32:36 +0300

    Restore Interface Segregation Principle

--- 1 SOLID/4 Interface Segregation/4 Interface Segregation/Program.cs
+++ 1 SOLID/4 Interface Segregation/4 Interface Segregation/Program.cs
@@ -10,14 +10,59 @@ namespace _4_Interface_Segregation
             public string Body { get; set; }
         }
 
-        public interface IMachine
+        public interface IMachine : IPrinter, IScanner, IFax {}
+
+        public interface IPrinter
         {
             void Print(Document d);
+        }
+
+        public interface IScanner
+        {
             void Scan(Document d);
+        }
+
+        public interface IFax
+        {
             void Fax(Document d);
         }
 
         public class MultifunctionPrinter : IMachine
+        {
+            private IPrinter _printer;
+            private IScanner _scanner;
+
+            public MultifunctionPrinter(IPrinter printer, IScanner scanner)
+            {
+                _printer = printer;
+                _scanner = scanner;
+            }
+
+            public void Print(Document d)
+            {
+                _printer.Print(d);   //  decorator pattern
+            }
+
+            public void Scan(Document d)
+            {
+                _scanner.Scan(d);   //  decorator pattern
+            }
+
+            public void Fax(Document d)
+            {
+                //  Fax document
+            }
+        }
+
+        public class OldFashionedPrinter : IPrinter
+        {
+            public void Print(Document d)
+            {
+                //  Print document
+            }
+        }
+
+        public class Photocopier : IPrinter, IScanner
         {
             public void Print(Document d)
             {
@@ -27,31 +72,6 @@ namespace _4_Interface_Segregation
             public void Scan(Document d)
             {
                 //  Scan document
-            }
-
-            public void Fax(Document d)
-            {
-                //  Fax document
-            }
-        }
-
-        public class OldFashionedPrinter : IMachine
-        {
-            public void Print(Document d)
-            {
-                //  Print document
-            }
-
-            public void Scan(Document d)
-            {
-                //  Nothing to do here
-                throw new NotImplementedException();
-            }
-
-            public void Fax(Document d)
-            {
-                //  Nothing to do here
-                throw new NotImplementedException();
             }
         }
 
