4aff195b37e46445d3ac6ccd2b911c49200c8f9d
AlexL70 <alexander.levinson.70@gmail.com>
2018-07-22 17:22:39 +0300

    SOLID: Break Interface Segregation Principle

---
+++ 1 SOLID/4 Interface Segregation/4 Interface Segregation/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _4_Interface_Segregation
+{
+    class Program
+    {
+        public class Document
+        {
+            public string Header { get; set; }
+            public string Body { get; set; }
+        }
+
+        public interface IMachine
+        {
+            void Print(Document d);
+            void Scan(Document d);
+            void Fax(Document d);
+        }
+
+        public class MultifunctionPrinter : IMachine
+        {
+            public void Print(Document d)
+            {
+                //  Print document
+            }
+
+            public void Scan(Document d)
+            {
+                //  Scan document
+            }
+
+            public void Fax(Document d)
+            {
+                //  Fax document
+            }
+        }
+
+        public class OldFashionedPrinter : IMachine
+        {
+            public void Print(Document d)
+            {
+                //  Print document
+            }
+
+            public void Scan(Document d)
+            {
+                //  Nothing to do here
+                throw new NotImplementedException();
+            }
+
+            public void Fax(Document d)
+            {
+                //  Nothing to do here
+                throw new NotImplementedException();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
