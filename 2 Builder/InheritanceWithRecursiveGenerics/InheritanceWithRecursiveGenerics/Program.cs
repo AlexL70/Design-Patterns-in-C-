bf4c216aad0c42b79a17b9055ef17ef7c5ebf960
AlexL70 <alexander.levinson.70@gmail.com>
2018-09-07 18:19:36 +0300

    13. Fluent Builder Inheritance with Recursive Generics

---
+++ 2 Builder/InheritanceWithRecursiveGenerics/InheritanceWithRecursiveGenerics/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace InheritanceWithRecursiveGenerics
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+        }
+
+        public class Builder : PersonJobBuilder<Builder> { }
+
+        public static Builder Create = new Builder();
+    }
+
+    public class PersonBuilder
+    {
+        protected Person person = new Person();
+
+        public Person Build()
+        {
+            return person;
+        }
+    }
+
+    public class PersonInfoBuilder<SELF> : PersonBuilder
+        where SELF : PersonInfoBuilder<SELF>
+    {
+        public SELF Called(string name)
+        {
+            person.Name = name;
+            return (SELF)this;
+        }
+    }
+
+    public class PersonJobBuilder<SELF> : PersonInfoBuilder<PersonJobBuilder<SELF>>
+        where SELF : PersonJobBuilder<SELF>
+    {
+        public SELF WorkAsA(string position)
+        {
+            person.Position = position;
+            return (SELF)this;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Person.Create.Called("Jack").WorkAsA("postman").Build());
+        }
+    }
+}
