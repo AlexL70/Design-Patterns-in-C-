17de83a2cecaf835d2e43649cdfd08d1222f3da9
AlexL70 <alexander.levinson.70@gmail.com>
2018-09-07 20:48:43 +0300

    Builder coding exercise

---
+++ 2 Builder/CodingExercise/CodingExercise/CodeBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Coding.Exercise
+{
+    public class CodeBuilder
+    {
+        private string _className;
+        private List<Field> fields = new List<Field>();
+
+        internal class Field
+        {
+            public string Name { get; set; }
+            public string Type { get; set; }
+
+            public override string ToString()
+            {
+                return $"  public {Type} {Name};";
+            }
+        }
+
+        public CodeBuilder(string className)
+        {
+            _className = className;
+        }
+
+        public CodeBuilder AddField(string name, string type)
+        {
+            fields.Add(new Field() { Name = name, Type = type});
+            return this;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"public class {_className}");
+            sb.AppendLine("{");
+            foreach (var field in fields)
+            {
+                sb.AppendLine(field.ToString());
+            }
+            sb.AppendLine("}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ 2 Builder/CodingExercise/CodingExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cb = new CodeBuilder("Person").AddField("Name", "string").AddField("Age", "int");
+            Console.WriteLine(cb);
+        }
+    }
+}
