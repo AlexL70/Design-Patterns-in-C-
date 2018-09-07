a99c495554e718da05b36db3d5bf3096ca340b88
AlexL70 <alexander.levinson.70@gmail.com>
2018-09-07 15:14:09 +0300

    10. Life Without Builder

---
+++ 2 Builder/LifeWithoutBuilder/LifeWithoutBuilder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace LifeWithoutBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = "hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            Console.WriteLine(sb);
+
+            var words = new string[] { "hello", "world"};
+            sb.Clear();
+            sb.Append("<ul>");
+            foreach (var word in words)
+            {
+                sb.Append("<li>");
+                sb.Append(word);
+                sb.Append("</li>");
+            }
+            sb.Append("</ul>");
+            Console.WriteLine(sb);
+        }
+    }
+}
