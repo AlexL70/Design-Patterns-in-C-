5e505205c238a85fb7e0ecebf49772bc45bbd5a3
AlexL70 <alexander.levinson.70@gmail.com>
2018-09-11 21:31:10 +0300

    Coding Exercise 2: Factory Coding Exercise

---
+++ 3 Factory/Coding.Exercise/Coding.Exercise/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Coding.Exercise
+{
+    class Program
+    {
+        public class Person
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+            public override string ToString() => $"Id = {Id}, Name = {Name}";
+        }
+
+        public class PersonFactory
+        {
+            private int _index;
+
+            public Person CreatePerson(string name)
+            {
+                return new Person {Id = _index++, Name = name};
+            }
+
+            public PersonFactory()
+            {
+                _index = 0;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var factory = new PersonFactory();
+            List<Person> persons = new List<Person>
+            {
+                factory.CreatePerson("John"),
+                factory.CreatePerson("Jack"),
+                factory.CreatePerson("Suzy")
+            };
+            foreach (var p in persons)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
