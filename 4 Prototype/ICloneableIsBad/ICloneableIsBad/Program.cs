064ac683c9ea1774909a0ee0690a030316a952b2
AlexL70 <alexander.levinson.70@gmail.com>
2018-09-14 11:51:12 +0300

    25. ICloneable is Bad

---
+++ 4 Prototype/ICloneableIsBad/ICloneableIsBad/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ICloneableIsBad
+{
+    public class Person : ICloneable
+    {
+        public string[] Names { get; set; }
+        public Address Address { get; set; }
+
+        public Person(string[] names, Address address)
+        {
+            Names = names ?? throw new ArgumentNullException(nameof(names));
+            Address = address ?? throw new ArgumentNullException(nameof(address));
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Names)}: {string.Join(" ", Names)}, {nameof(Address)}: {Address}";
+        }
+
+        public object Clone()
+        {
+            return new Person( (string[]) Names.Clone(), (Address) Address.Clone());
+        }
+    }
+
+    public class Address : ICloneable
+    {
+        public string StreetName { get; set; }
+        public int HouseNumber { get; set; }
+
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName ?? throw new ArgumentNullException(nameof(streetName));
+            HouseNumber = houseNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+        }
+
+        public object Clone()
+        {
+            return new Address(StreetName, HouseNumber);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person( new []{ "John", "Smith"}, new Address("London Road", 123));
+            Person jane = (Person)john.Clone();
+            jane.Names[0] = "Jane";
+            jane.Address.HouseNumber = 321;
+            Console.WriteLine(john);
+            Console.WriteLine(jane);
+        }
+    }
+}
