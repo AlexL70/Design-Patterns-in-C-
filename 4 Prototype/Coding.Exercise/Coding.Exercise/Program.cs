7311da0ef8e5a498a88f16cac5bd61b4ba3ff628
AlexL70 <alexander.levinson.70@gmail.com>
2018-09-14 13:27:04 +0300

    Prototype coding exercise

---
+++ 4 Prototype/Coding.Exercise/Coding.Exercise/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace Coding.Exercise
+{
+    public class Point
+    {
+        public int X, Y;
+
+        public override string ToString()
+        {
+            return $"{nameof(X)}: {X}, {nameof(Y)}: {Y}";
+        }
+    }
+
+    public class Line
+    {
+        public Point Start, End;
+
+        public Line DeepCopy()
+        {
+            return new Line
+            {
+                Start = new Point() {X = this.Start.X, Y = this.Start.Y},
+                End = new Point() {X = this.End.X, Y = this.End.Y}
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Start)}: {Start}, {nameof(End)}: {End}";
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var p1 = new Line();
+            p1.Start = new Point() {X = 1, Y = 2};
+            p1.End = new Point() {X = 3, Y = 4};
+            var p2 = p1.DeepCopy();
+            p2.End.Y = 6;
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+
+}
