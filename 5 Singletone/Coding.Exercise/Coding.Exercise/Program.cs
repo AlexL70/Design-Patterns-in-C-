c32defa27a8c61a99125666a8bba6edca85256f6
AlexL70 <alexander.levinson.70@gmail.com>
2018-09-18 00:09:53 +0300

    Singleton Coding Exercise

---
+++ 5 Singletone/Coding.Exercise/Coding.Exercise/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+
+namespace Coding.Exercise
+{
+    class Program
+    {
+        public interface IFactory<T>
+        {
+            T Create();
+        }
+
+        public class ObjectFactory : IFactory<object>
+        {
+            public object Create()
+            {
+                return new object();
+            }
+        }
+
+        public class SingletonObjectFactory : IFactory<object>
+        {
+            private static object _obj = null;
+
+            public object Create()
+            {
+                if (_obj == null)
+                {
+                    _obj = new object();
+                }
+
+                return _obj;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var simpleFactory = new ObjectFactory();
+            WriteLine(
+                $"Is {nameof(simpleFactory)} a singleton? It is {SingletonTester.IsSingleton(simpleFactory.Create)}.");
+            var singletonFactory = new SingletonObjectFactory();
+            WriteLine($"Is {nameof(singletonFactory)} a singleton? If is {SingletonTester.IsSingleton(singletonFactory.Create)}");
+        }
+    }
+}
---
+++ 5 Singletone/Coding.Exercise/Coding.Exercise/SingletonTester.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    class SingletonTester
+    {
+        public static bool IsSingleton(Func<object> func)
+        {
+            var inst1 = func();
+            var inst2 = func();
+            return inst1 == inst2;
+        }
+    }
+}
