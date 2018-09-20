5cc9a5f17fd656eab5d747a81471e3d35c02b975
AlexL70 <alexander.levinson.70@gmail.com>
2018-09-20 14:29:09 +0300

    52. Static Decorator Composition (is not good enough)

--- 9 Decorator/DynamicDecoratorComposition/DynamicDecoratorComposition/Program.cs
+++ 9 Decorator/DynamicDecoratorComposition/DynamicDecoratorComposition/Program.cs
@@ -3,64 +3,99 @@ using static System.Console;
 
 namespace DynamicDecoratorComposition
 {
-    public interface IShape
+    public abstract class Shape
     {
-        string AsString();
+        public abstract string AsString();
     }
 
-    public class Circle : IShape
+    public class Circle : Shape
     {
         private float _radius;
+
+        public Circle() : this(1f) { }
 
         public Circle(float radius) => _radius = radius;
 
         public void Resize(float factor) => _radius *= factor;
 
-        public string AsString() => $"A circle with radius {_radius}";
+        public override string AsString() => $"A circle with radius {_radius}";
     }
 
-    public class Square : IShape
+    public class Square : Shape
     {
         private float _side;
 
         public Square(float side) => _side = side;
 
-        public string AsString() => $"A square with side {_side}";
+        public Square() : this(1f) { }
+
+        public override string AsString() => $"A square with side {_side}";
     }
 
-    public class ColoredShape : IShape
+    public class ColoredShape : Shape
     {
-        private IShape _shape;
+        private Shape _shape;
         private string _color;
 
-        public ColoredShape(IShape shape, string color)
+        public ColoredShape(Shape shape, string color)
         {
             _shape = shape ?? throw new ArgumentNullException(nameof(shape));
             _color = color ?? throw new ArgumentNullException(nameof(color));
         }
 
-        public string AsString() => $"{_shape.AsString()} colored with {_color}";
+        public override string AsString() => $"{_shape.AsString()} colored with {_color}";
     }
 
-    public class TransparentShape : IShape
+    public class TransparentShape : Shape
     {
-        private IShape _shape;
+        private Shape _shape;
         private float _transparency;
 
-        public TransparentShape(IShape shape, float transparency)
+        public TransparentShape(Shape shape, float transparency)
         {
             _shape = shape ?? throw new ArgumentNullException(nameof(shape));
             _transparency = transparency;
         }
 
-        public string AsString() => $"{_shape.AsString()} transparent of {_transparency * 100.0} percent";
+        public override string AsString() => $"{_shape.AsString()} transparent of {_transparency * 100.0} percent";
     }
 
-    class Program
+    public class ColoredShape<T> : Shape where T : Shape, new()
     {
-        static void Main(string[] args)
+        private string _color;
+        private T _shape = new T();
+
+        public ColoredShape() : this("black") { }
+
+        public ColoredShape(string color)
         {
-            var square = new Square(1.23f);
+            _color = color ?? throw new ArgumentNullException(nameof(color));
+        }
+
+        public override string AsString() => $"{_shape.AsString()} colored with {_color}";
+    }
+
+    public class TransparentShape<T> : Shape where T : Shape, new()
+    {
+        private float _transparency;
+        private T _shape = new T();
+
+        public TransparentShape() : this(0) { }
+
+        public TransparentShape(float transparency) => _transparency = transparency;
+
+        public override string AsString() => $"{_shape.AsString()} transparent of {_transparency * 100.0} percent";
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Shape square = new ColoredShape<Square>("red");
+            WriteLine(square.AsString());
+            Shape trCircle = new TransparentShape<ColoredShape<Circle>>(0.25f);
+            WriteLine(trCircle.AsString());
+            square = new Square(1.23f);
             WriteLine(square.AsString());
             var redSquare = new ColoredShape(square, "red");
             WriteLine(redSquare.AsString());
