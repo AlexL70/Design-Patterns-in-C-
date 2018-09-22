c30ec7a81e250200bf6ddce444f288ed1f7fe4f6
AlexL70 <alexander.levinson.70@gmail.com>
2018-09-22 12:22:57 +0300

    64 Dynamic Proxy for Logging - starting point

---
+++ 12 Proxy/DynamicProxyForLogging/DynamicProxyForLogging/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using static System.Console;
+
+namespace DynamicProxyForLogging
+{
+    public interface IBankAccount
+    {
+        void Deposit(int amount);
+        bool Withdraw(int amount);
+        string ToString();
+    }
+
+    public class BankAccount : IBankAccount
+    {
+        private int _balance;
+        private int _overdraftLimit = -500;
+        public int Balance => _balance;
+
+        public void Deposit(int amount)
+        {
+            _balance += amount;
+            WriteLine($"Deposited {amount}, balance is now {_balance}");
+        }
+
+        public bool Withdraw(int amount)
+        {
+            if (_balance - amount > _overdraftLimit)
+            {
+                _balance -= amount;
+                WriteLine($"Withdrew {amount}, balance is now {_balance}");
+                return true;
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Balance)}: {Balance}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ba = new BankAccount();
+            ba.Deposit(100);
+            ba.Withdraw(50);
+            WriteLine(ba);
+        }
+    }
+}
