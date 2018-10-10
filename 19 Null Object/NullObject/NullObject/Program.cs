0cb13539110919d74639aed1b3ca5d92e91e93a9
AlexL70 <alexander.levinson.70@gmail.com>
2018-10-10 16:28:45 +0300

    98. Dynamic null object

--- 19 Null Object/NullObject/NullObject/Program.cs
+++ 19 Null Object/NullObject/NullObject/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Dynamic;
 using Autofac;
+using ImpromptuInterface;
 using static System.Console;
 
 namespace NullObject
@@ -30,6 +32,18 @@ namespace NullObject
         public void Warn(string msg) {}
     }
 
+    public class Null<TInterface> : DynamicObject where TInterface : class
+    {
+        public static TInterface Instance => 
+            new Null<TInterface>().ActLike<TInterface>();
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            result = Activator.CreateInstance(binder.ReturnType);
+            return true;
+        }
+    }
+
     public class BankAccount
     {
         private readonly ILog _log;
@@ -52,18 +66,29 @@ namespace NullObject
     {
         static void Main(string[] args)
         {
+            //  Use normal call
             var log = new ConsoleLog();
             var ba = new BankAccount(log, 100);
             ba.Deposit(100);
+            //  Use static null object instead as ILog
             var cb = new ContainerBuilder();
             cb.RegisterType<BankAccount>();
             cb.RegisterType<NullLog>().As<ILog>();
             using (var c = cb.Build())
             {
+                WriteLine();
+                WriteLine("Use static null object");
                 ba = c.Resolve<BankAccount>();
                 ba.Deposit(300);
                 WriteLine($"Current balance is {ba.Balance}");
             }
+            //  Use dynamic null object as ILog
+            WriteLine();
+            WriteLine("Use dynamic null object");
+            var dnLog = Null<ILog>.Instance;
+            ba = new BankAccount(dnLog);
+            ba.Deposit(500);
+            WriteLine($"Current balance is {ba.Balance}");
         }
     }
 }
