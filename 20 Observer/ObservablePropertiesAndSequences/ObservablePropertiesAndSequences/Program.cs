60a416391a22dc53a29b1069540d90c9acfafd7e
AlexL70 <alexander.levinson.70@gmail.com>
2018-10-11 15:54:31 +0300

    103. Observable Properties and Sequences: built-in event approach

---
+++ 20 Observer/ObservablePropertiesAndSequences/ObservablePropertiesAndSequences/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace ObservablePropertiesAndSequences
+{
+    public class Market
+    {
+        private List<float> _prices = new List<float>();
+
+        public void AddPrice(float price)
+        {
+            _prices.Add(price);
+            PriceAdded?.Invoke(this, price);
+        }
+
+        public event EventHandler<float> PriceAdded;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var market = new Market();
+            market.PriceAdded += (sender, price) => { WriteLine($"Price added {price}"); };
+            market.AddPrice(123);
+        }
+    }
+}
